61d7107b4492208d76453691cba62a6f1fffa17b
root <root@DESKTOP-EJU2C8I.localdomain>
2022-09-19 23:21:53 -0400

    finish set up react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { render } from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { StrictMode } from 'react';
 import SearchParams from "./SearchParams";
+import Details from './Details';
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>
   )
 }
 
---
+++ src/Details.js
@@ -0,0 +1,7 @@
+
+
+const Details = () => {
+    return <h2> hi </h2>
+};
+
+export default Details;
