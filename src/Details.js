1abc8aa22db1bbd938b1acfc72da0b5e064262e6
root <root@DESKTOP-EJU2C8I.localdomain>
2022-09-20 22:02:54 -0400

    finish error boundary

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import { useParams } from "react-router-dom";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
 class Details extends Component {
     state = { loading: true };
@@ -18,6 +19,8 @@ class Details extends Component {
         if (this.state.loading) {
             return <h2>loading ...</h2>
         }
+
+        throw new Error("new error")
 
         const {animal, breed, city, state, description, name, images } = this.state;
 
@@ -39,7 +42,11 @@ class Details extends Component {
 
 const WrappedDetails = () => {
     const params = useParams();
-    return <Details params={params} />
+    return (
+        <ErrorBoundary>
+            <Details params={params} />
+        </ErrorBoundary>
+    )
 }
 
 export default WrappedDetails;
