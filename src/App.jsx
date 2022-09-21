1959a837dc434dcf0128298438ce7f3a76bf242f
root <root@DESKTOP-EJU2C8I.localdomain>
2022-09-20 21:37:57 -0400

    finish React Capabilities

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { render } from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { StrictMode } from 'react';
 import SearchParams from "./SearchParams";
 import Details from './Details';
@@ -8,6 +8,9 @@ const App = () => {
   return (
     <StrictMode>
       <BrowserRouter>
+      <header>
+        <Link to="/">Adopt Me!</Link>
+      </header>
         <Routes>
           <Route path="/details/:id" element={<Details />} />
           <Route path="/" element={<SearchParams />} />
